ec00cb5a81582ad3f9b02803650b96bd598db5ff
Mbiyrukzz <mbiyrukzz@gmail.com>
2024-01-03 16:02:36 +0300

    About

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import { Heading } from '@radix-ui/themes'
 import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div id="#about">
+    <div
+      ref={AboutUs}
+      id="#about"
+      className="bg-[#8338ec] rounded-lg p-3 outline-offset-2 text-white text-center"
+    >
       <Heading>About Us</Heading>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <main className="">
       <MainContent />
       <ActionButtons />
+      <AboutUs />
     </main>
   )
 }
