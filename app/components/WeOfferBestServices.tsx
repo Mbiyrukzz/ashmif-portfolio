55e26cd35596f492e94d0c584c4721643f1317df
Mbiyrukzz <mbiyrukzz@gmail.com>
2024-01-12 17:54:48 +0300

    Minor changes

--- app/components/WeOfferBestServices.tsx
+++ app/components/WeOfferBestServices.tsx
@@ -13,18 +13,14 @@ const WeOfferBestServices = () => {
         </Heading>
       </Box>
       <Box>
-        <Card size="2" style={{ maxWidth: 300 }} className="mt-2">
+        <Card size="2" style={{ maxWidth: 400 }} className="mt-2">
           <Inset clip="padding-box" side="top" pb="current">
             <Image
-              src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+              className="p-1 flex items-center text-center rounded-t-xl"
+              src="/smartfreight.png"
               alt="Bold typography"
-              style={{
-                display: 'block',
-                objectFit: 'cover',
-                width: '100%',
-                height: 140,
-                backgroundColor: 'var(--gray-5)',
-              }}
+              width="400"
+              height="140"
             />
           </Inset>
           <Text as="p" size="3">
